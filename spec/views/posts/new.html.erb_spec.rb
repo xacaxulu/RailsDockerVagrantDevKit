5cc229c9807b6b0d318bba3f6172738ce9e88c9f
James Denman <james@fastmail.se>
2015-12-07 16:23:50 -0500

    added some assets; reworked routes

---
+++ spec/views/posts/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "posts/new", type: :view do
+  before(:each) do
+    assign(:post, Post.new(
+      :title => "MyString",
+      :description => "MyText"
+    ))
+  end
+
+  it "renders new post form" do
+    render
+
+    assert_select "form[action=?][method=?]", posts_path, "post" do
+
+      assert_select "input#post_title[name=?]", "post[title]"
+
+      assert_select "textarea#post_description[name=?]", "post[description]"
+    end
+  end
+end
